034e24fa7951a8d1aeb2e2f40078b731351a6f05
chaohuichen <chenpeter0821@gmail.com>
2020-06-15 15:20:53 -0400

    move the start btn to the header

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { colorize, paintPath } from '../store/store';
-import { bfs } from './BFS';
-import { sleep } from './ult';
 
 class Grid extends Component {
   constructor() {
@@ -19,40 +17,14 @@ class Grid extends Component {
       this.props.paint(row, col);
     }
   }
-  search = async (grid, start) => {
-    let ans = await bfs(grid, start, this.props.paint);
-    //north,south west    east
-    let direction = {
-      North: [-1, 0],
-      South: [1, 0],
-      West: [0, -1],
-      East: [0, 1],
-    };
 
-    let startrow = start[0];
-    let startcol = start[1];
-    for (let i = 0; i < ans.length; ++i) {
-      let dr = direction[ans[i]];
-      startrow += dr[0];
-      startcol += dr[1];
-      this.props.paintPath(startrow, startcol);
-      await sleep(100);
-    }
-    // console.log(this.props.grid);
-  };
   render() {
-    const { grid, start, end } = this.props;
+    const { grid } = this.props;
 
     return (
       <div id='pixelate'>
         {/* truncated for brevity... */}
-        {/* bfs(grid, start, end, this.props.paint) */}
-        <button
-          style={{ width: 50, height: 50 }}
-          onClick={() => this.search(grid, start)}
-        >
-          Start
-        </button>
+
         <table
           onMouseDown={() => this.setState({ paint: true })}
           onMouseUp={() => this.setState({ paint: false })}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,56 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { clear } from '../store/store';
+import { clear, colorize, paintPath } from '../store/store';
+import { bfs } from './BFS';
+import { sleep } from './ult';
 import headerStyle from './header.module.css';
+
 class Header extends React.Component {
-  constructor() {
-    super();
-  }
+  search = async (grid, start) => {
+    let ans = await bfs(grid, start, this.props.paint);
+    //north,south west    east
+    let direction = {
+      North: [-1, 0],
+      South: [1, 0],
+      West: [0, -1],
+      East: [0, 1],
+    };
+
+    let startrow = start[0];
+    let startcol = start[1];
+    for (let i = 0; i < ans.length; ++i) {
+      let dr = direction[ans[i]];
+      startrow += dr[0];
+      startcol += dr[1];
+      this.props.paintPath(startrow, startcol);
+      await sleep(100);
+    }
+    // console.log(this.props.grid);
+  };
   render() {
+    const { grid, start } = this.props;
     return (
       <div className={headerStyle.header}>
         <h3>PathFinding Visualizer</h3>
         <button onClick={this.props.clear}>Clear</button>
+        <button onClick={() => this.search(grid, start)}>BFS Start</button>
       </div>
     );
   }
 }
-
+const mapState = (state) => {
+  return {
+    grid: state.grid,
+    start: state.start,
+    end: state.end,
+  };
+};
 const mapDispatch = (dispatch) => {
   return {
     clear: () => dispatch(clear()),
+    paint: (row, col) => dispatch(colorize(row, col)),
+    paintPath: (row, col) => dispatch(paintPath(row, col)),
   };
 };
 
-export default connect(null, mapDispatch)(Header);
+export default connect(mapState, mapDispatch)(Header);
